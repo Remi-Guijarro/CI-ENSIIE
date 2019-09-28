74138d62a2b3dae19447600a007a199143d14857
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-09-28 21:38:18 +0200

    Try to improve Better Code Hub score

--- src/BookValidator.js
+++ src/BookValidator.js
@@ -1,13 +1,9 @@
 class BookValidator {
     isBook(book){
-        if(undefined === book || null === book){
-           return false;
-        }else {
-            return this.isArgumentDefined(book.id) &&
+        return undefined !== book && this.isArgumentDefined(book.id) &&
             this.isArgumentDefined(book.name) &&
             this.isArgumentDefined(book.price) &&
             this.isArgumentDefined(book.added_at);
-        }
     }
 
     isArgumentDefined(argument){
