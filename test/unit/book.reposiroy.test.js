5ad3cc150cf54dd1ae77256226eb1a910883cadb
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-09-28 22:11:16 +0200

    Changed tests location from src to test/unit

--- test/unit/book.reposiroy.test.js
+++ test/unit/book.reposiroy.test.js
@@ -1,4 +1,4 @@
-const BookRepository = require('./book.repository');
+const BookRepository = require('../../src/book.repository');
 
 describe('Book repository Save', function () {
     test('Save a book', () => {
